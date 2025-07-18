2c514ce92397f7a56a0bcaaaf71b084ad89dc199
Dima-Mendoza <spartanskijmapper534@gmail.com>
2025-07-18 22:43:41 +0300

    Encryption and logger

---
+++ src/logger.py
@@ -0,0 +1,14 @@
+from loguru import logger
+import os
+
+os.makedirs("logs", exist_ok=True)
+
+logger.remove()
+logger.add(
+    "logs/app.log",
+    rotation="1 MB",
+    retention="10 days",
+    level="INFO",
+    encoding="utf-8",
+    enqueue=True
+)
